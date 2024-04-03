141bc16e73d7d90a5b76ca47ea6c8db0ade473d3
Henrique Rosa <samuel_kuik@hotmail.com>
2024-04-03 15:43:47 -0300

    feat: express setup

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,1 +1,12 @@
-console.log('hello world');
+import express from 'express';
+import {config} from 'dotenv';
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.get('/',(_req, res) => res.send('Hello World!') );
